48b5506d46df59a856a7acc1bd59b71a842826c9
imai.tetsu <imai.tetsu@canon.co.jp>
2019-05-29 15:18:45 +0900

    upd path

--- precache-manifest.55e41737be3f839498b83ab62225ca6d.js
+++ precache-manifest.55e41737be3f839498b83ab62225ca6d.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "c7ec666a219158efb251",
-    "url": "/static/css/main.2cce8147.chunk.css"
+    "url": "/records/static/css/main.2cce8147.chunk.css"
   },
   {
     "revision": "ef430fc5c5896c55526b",
-    "url": "/static/js/2.31d20c7b.chunk.js"
+    "url": "/records/static/js/2.31d20c7b.chunk.js"
   },
   {
     "revision": "c7ec666a219158efb251",
-    "url": "/static/js/main.42eb5302.chunk.js"
+    "url": "/records/static/js/main.42eb5302.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "/records/static/js/runtime~main.a8a9905a.js"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "url": "/records/static/media/logo.5d5d9eef.svg"
   }
 ]);
